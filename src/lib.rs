295d09de8dcf976d07d264d4b5fd78a32b342319
saturnavt <saturnavt@gmail.com>
2022-12-03 00:48:34 -0500

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,59 @@
-#![allow(unused)] // silence unused warning
-
 use serde::{Deserialize, Serialize};
 
 // Games struct
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Games {
-  title: String,
-  img: String,
-  main: String,
-  extra: String,
-  completionist: String,
+    title: String,
+    img: String,
+    main: String,
+    extra: String,
+    completionist: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 struct HowlongtobeatResponse<Object> {
-  data: Vec<Object>,
+    data: Vec<Object>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 pub struct Howlongtobeat {
-  pub game_image: String,
-  pub game_name: String,
-  pub comp_main: i32,
-  pub comp_plus: i32,
-  pub comp_100: i32,
+    pub game_image: String,
+    pub game_name: String,
+    pub comp_main: i32,
+    pub comp_plus: i32,
+    pub comp_100: i32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 pub struct Game {
-  pub image: String,
-  pub title: String,
-  pub main: String,
-  pub extra: String,
-  pub completionist: String,
+    pub image: String,
+    pub title: String,
+    pub main: String,
+    pub extra: String,
+    pub completionist: String,
 }
 
 pub fn search(game: String) -> Vec<Game> {
-  let games_found = howlongtobeat(game.to_string()).unwrap();
+    let games_found: Vec<Game> = howlongtobeat(game.to_string()).unwrap();
 
-  return games_found;
+    return games_found;
 }
 
 fn seconds_to_time(time: i32) -> String {
-  let hours: i32 = time / 3600;
-  let minutes: i32 = time % 3600 / 60;
+    let hours: i32 = time / 3600;
+    let minutes: i32 = time % 3600 / 60;
 
-  return hours.to_string() + "h " + &minutes.to_string() + "m";
+    return hours.to_string() + "h " + &minutes.to_string() + "m";
 }
 
 fn line_to_words(line: &str) -> Vec<String> {
-  line.split_whitespace().map(str::to_string).collect()
+    line.split_whitespace().map(str::to_string).collect()
 }
 
 #[tokio::main]
 async fn howlongtobeat(game: String) -> Result<Vec<Game>, reqwest::Error> {
-  // Post request to howlongtobeat
-  let response: serde_json::Value = reqwest::Client::new()
+    // Post request to howlongtobeat
+    let response: serde_json::Value = reqwest::Client::new()
         .post("https://www.howlongtobeat.com/api/search")
         .json(&serde_json::json!(
             {
@@ -100,20 +98,24 @@ async fn howlongtobeat(game: String) -> Result<Vec<Game>, reqwest::Error> {
         .json()
         .await?;
 
-  let json_games_found: HowlongtobeatResponse<Howlongtobeat> =
-    serde_json::from_str(&response.to_string()).expect("JSON was not well-formatted");
+    let json_games_found: HowlongtobeatResponse<Howlongtobeat> =
+        serde_json::from_str(&response.to_string()).expect("JSON was not well-formatted");
 
-  let mut games = vec![];
+    let mut games: Vec<Game> = vec![];
 
-  for elem in json_games_found.data {
-    games.push(Game {
-      image: String::from("https://howlongtobeat.com/games/".to_owned() + &elem.game_image),
-      title: String::from(elem.game_name),
-      main: seconds_to_time(elem.comp_main),
-      extra: seconds_to_time(elem.comp_plus),
-      completionist: seconds_to_time(elem.comp_100),
-    })
-  }
-  // Return response from howlongtobeat
-  return Ok(games);
+    for elem in json_games_found.data {
+        games.push(Game {
+            image: String::from("https://howlongtobeat.com/games/".to_owned() + &elem.game_image),
+            title: String::from(elem.game_name),
+            main: seconds_to_time(elem.comp_main),
+            extra: seconds_to_time(elem.comp_plus),
+            completionist: seconds_to_time(elem.comp_100),
+        })
+    }
+    // Return response from howlongtobeat
+    return Ok(games);
 }
+
+//test
+#[cfg(test)]
+mod test;
--- src/test.rs
+++ src/test.rs
@@ -1,8 +1,27 @@
-use howlongtobeat;
+use super::*;
 
 #[test]
 fn test_search() {
-    let games = howlongtobeat::search("Elden Ring".to_string());
-    println!("{:?}", games);
-    assert_eq!(games.len(), 1);
+    let games: Vec<Game> = search("Elden Ring".to_string());
+
+    let expected: [Game; 2] = [
+        Game {
+            image: String::from("https://howlongtobeat.com/games/68151_Elden_Ring.jpg".to_owned()),
+            title: String::from("Elden Ring"),
+            main: String::from("54h 22m"),
+            extra: String::from("98h 55m"),
+            completionist: String::from("132h 19m"),
+        },
+        Game {
+            image: String::from(
+                "https://howlongtobeat.com/games/108888_Elden_Ring_GB.jpg".to_owned(),
+            ),
+            title: String::from("Elden Ring GB"),
+            main: String::from("0h 19m"),
+            extra: String::from("0h 29m"),
+            completionist: String::from("0h 0m"),
+        },
+    ];
+
+    assert_eq!(games, expected);
 }
